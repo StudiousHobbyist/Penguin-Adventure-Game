9e9ac2f1b23d4085e8566a57df8e8a8c97445cf1
Marcus Morgan <adorableaxolotl999@gmail.com>
2026-09-14 14:19:16 -0500

    More Cleanup

---
+++ Penguin Adventure Game/gui.h
@@ -0,0 +1,79 @@
+#pragma once
+
+#include <cmath>
+#include "C:\Users\morga\Documents\VS Includes\raylib-6.0_win64_msvc16\include\raylib.h"
+
+
+Vector2 Normalize(Vector2 v)
+{
+    float length = std::sqrt(v.x * v.x + v.y * v.y);
+
+    if (length == 0.0f)
+        return { 0, 0 };
+
+    return {
+        v.x / length,
+        v.y / length
+    };
+}
+
+struct ProgressBar {
+    int width;
+    int height;
+
+    float itemCount;   // current value
+    float itemTarget;   // max value
+
+    Color colorBg;
+    Color colorFg;
+};
+
+inline ProgressBar MakeProgressBar(
+    int width,
+    int height,
+
+    float itemCount,
+    float itemTarget,
+
+    Color colorBg = DARKGRAY,
+    Color colorFg = GREEN
+) {
+    return ProgressBar{
+        width,
+        height,
+        itemCount,
+        itemTarget,
+        colorBg,
+        colorFg
+    };
+}
+
+void DrawProgressBar(const ProgressBar& bar, Vector2 pos)
+{
+    // background
+    DrawRectangle((int)pos.x, (int)pos.y, bar.width, bar.height, bar.colorBg);
+
+    std::string text = std::to_string((int)bar.itemCount) + "/" + std::to_string((int)bar.itemTarget);
+
+
+
+    // avoid divide-by-zero
+    float t = (bar.itemTarget > 0)
+        ? (float)bar.itemCount / (float)bar.itemTarget
+        : 0.0f;
+
+    if (t > 1.0f) t = 1.0f;
+    if (t < 0.0f) t = 0.0f;
+
+    // foreground fill
+    DrawRectangle(
+        (int)pos.x,
+        (int)pos.y,
+        (int)(bar.width * t),
+        bar.height,
+        bar.colorFg
+    );
+
+    DrawText(text.c_str(), (int)pos.x, (int)pos.y, bar.height, BLACK);
+}
+
--- Penguin Adventure Game/main.cpp
+++ Penguin Adventure Game/main.cpp
@@ -1,18 +1,128 @@
+// main.cpp
 
-#include <iostream>
 #include "C:\Users\morga\Documents\VS Includes\raylib-6.0_win64_msvc16\include\raylib.h"
+#include <string>
+#include <map>
+#include <array>
+
+#include "gui.h"
+#include "world.h"
+#include "entity.h"
+
+//will be changed once settings menu is added
+int TARGETFPS = 200;
+
+float WINDOWWIDTH = 800.0f;
+float WINDOWHEIGHT = 450.0f;
+
+float CAMERASPEED = 500.0f;
+
+float CAMERAZOOM = 1.0f;
+
+enum class CameraModeT {
+    Free,
+    Player
+};
 
 
+void input(Camera2D& camera, EntityManager& em, float dt) {
+    Entity* e = em.CurrentEntity();
 
-int main(void)
+    Vector2 move{ 0,0 };
+
+    if (IsKeyDown(KEY_A)) {
+        move.x = -1;
+    }
+    if (IsKeyDown(KEY_D)) {
+        move.x = 1;
+    }
+    if (IsKeyDown(KEY_W)) {
+        move.y = -1;
+    }
+    if (IsKeyDown(KEY_S)) {
+        move.y = 1;
+    }
+
+    if (move.x != 0 || move.y != 0)
+    {
+        move = Normalize(move);
+    }
+
+    if (e) {
+        e->Move(move.x * 10, move.y * 10, dt);
+        camera.target.x += (e->GetPos().x - camera.target.x) / (10 / camera.zoom);
+        camera.target.y += (e->GetPos().y - camera.target.y) / (10 / camera.zoom);
+    }
+
+
+    CAMERAZOOM += GetMouseWheelMove() * 2.0f * dt;
+
+    if (CAMERAZOOM < 0.05f)
+        CAMERAZOOM = 0.05f;
+
+    if (CAMERAZOOM > 10.0f)
+        CAMERAZOOM = 10.0f;
+
+    camera.zoom += (CAMERAZOOM - camera.zoom) / 10;
+}
+
+int main()
 {
-    InitWindow(800, 450, "raylib example - basic window");
+
+    InitWindow((int)WINDOWWIDTH, (int)WINDOWHEIGHT, "raylib example - basic window");
+
+    SetTargetFPS(TARGETFPS);
+
+    ProgressBar bar = MakeProgressBar(500, 50, 0, 1000);
+
+    float uiBuffer = 10;
+
+    EntityManager em;
+    ChunkManager cm;
+
+    em.AddEntity();
+
+    Camera2D camera = { 0 };
+
+    camera.target = { 0, 0 };
+    camera.offset = { WINDOWWIDTH / 2, WINDOWHEIGHT / 2 };
+    camera.rotation = 0.0f;
+    camera.zoom = 1.0f;
+
+    // ToggleFullscreen();
+    WINDOWWIDTH = GetScreenWidth();
+    WINDOWHEIGHT = GetScreenHeight();
+    camera.offset = { WINDOWWIDTH / 2, WINDOWHEIGHT / 2 };
 
     while (!WindowShouldClose())
     {
+        float dt = GetFrameTime();
+        // input
+
+        input(camera, em, dt);
+
+        cm.UpdateChunks(camera, 500.0);
+        cm.ProcessCompletedChunks(0.1);
+        cm.clean(camera, 5.0);
+
         BeginDrawing();
+
         ClearBackground(RAYWHITE);
-        DrawText("Congrats! You created your first window!", 190, 200, 20, LIGHTGRAY);
+
+        // render
+        BeginMode2D(camera);
+
+        cm.DrawChunks(camera);
+        em.DrawEntities(cm, dt);
+
+        EndMode2D();
+
+        // gui
+        DrawFPS(200, 0);
+
+        cm.DrawDiagnostics();
+        // DrawProgressBar(bar, { uiBuffer, uiBuffer });
+
         EndDrawing();
     }
 
